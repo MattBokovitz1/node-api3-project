3cee5bbbb1912caf149c6736bf03cfe19773f104
Matthew Bokovitz <MattBokovitz@Matthews-MBP-2.fios-router.home>
2021-01-13 19:48:36 -0500

    finished MVP

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,24 +1,50 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // do your magic!
+const Post = require("./posts-model");
+
+const { validatePostId } = require("../middleware/middleware");
+
+router.get("/", (req, res) => {
+  Post.get(req.query)
+    .then((posts) => {
+      res.json(posts);
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res.status(500).json({ message: "the posts could not be retrieved" });
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
-  // this needs a middleware to verify post id
+router.get("/:id", validatePostId, (req, res) => {
+  res.status(200).json(req.post);
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
-  // this needs a middleware to verify post id
+router.delete("/:id", validatePostId, (req, res) => {
+  Post.remove(req.params.id)
+    .then(() => {
+      res.status(200).json({ message: "the post has been removed " });
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res.status(500).json({ message: "error removing the post" });
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
-  // this needs a middleware to verify post id
+router.put("/:id", validatePostId, (req, res) => {
+  if (!req.body.text) {
+    res.status(400).json({ message: "please provide text for the post" });
+  }
+
+  Post.update(req.params.id, req.body)
+    .then((post) => {
+      res.status(200).json(post);
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res.status(500).json({ message: "error updating the post" });
+    });
 });
 
-// do not forget to export the router
+module.exports = router;
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// require your server and start it
+const server = require("./api/server");
+
+server.listen(4000, () => {
+  console.log("Server running on http://localhost:4000");
+});
